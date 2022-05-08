1e2817c0636344e991c343c2ec74d98994454566
Pikarin1121 <96364635+Pikarin1121@users.noreply.github.com>
2022-05-08 20:32:50 +0900

    Java基礎復習

---
+++ Java応用/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "‚Ð‚©‚é";
+		System.out.println(name);
+
+		name = "‚¿‚Î";
+		System.out.println(name);
+
+		final int NUM = 3;
+		System.out.println(NUM);
+	}
+}
